b3fedbf6fb0c8da1c2c620448bdac771271b7c86
Heqiaomao <965891161@qq.com>
2018-08-11 10:35:35 +0800

    jQuery
    jQuery

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Heqiaomao' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Heqiaomao',
+    miao: 'hewanying'
+  }); //循环读取到的内容
 });
 
 module.exports = router;
